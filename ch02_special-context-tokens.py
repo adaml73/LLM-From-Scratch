1948c9a5ad5d47c331a509ec8b19a3be3c341938
Adam Lyng <adamlyng73@gmail.com>
2026-05-21 22:17:16 -0400

    update

---
+++ ch02_special-context-tokens.py
@@ -0,0 +1,62 @@
+import re
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+tokenizer = SimpleTokenizerV1(vocab)
+
+text = """"It's the last he painted, you know," 
+           Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+tokenizer.decode(ids)
+print(tokenizer.decode(tokenizer.encode(text)))
+print(vocab)
+
+#helps the llm with context, and to handle unknown words
+#[BOS] - begining of sequence
+#[EOS] - end of sequence, helps created line between two different unrelated texts
+#[PAD] - padding - training may include batches of multiple texts, of different lengths. 
+#this pads shorter texts to the longest lenght so all texts are equal lenght
+#[UNK] - words no included in the vocabulary
+
+#this fails because hello is no in the vocab
+tokenizer = SimpleTokenizerV1(vocab)
+text = "Hello, do you like tea. Is this-- a test?"
+text.encode(text)
+
+
+
+
